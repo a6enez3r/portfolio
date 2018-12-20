544f09475079d68f3893aeae92365a34b3e7d814
Abenezer Mamo <abmamo@reed.edu>
2018-12-20 04:34:04 +0000

    added additional unit tests

--- test.py
+++ test.py
@@ -19,6 +19,10 @@ class BasicTest(unittest.TestCase):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    def test_nonexistent_page(self):
+        response = self.app.get('/azaz', follow_redirects=True)
+        self.assertEqual(response.status_code, 404)
+
 
 if __name__ == "__main__":
     unittest.main()
