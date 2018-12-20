3c52ef5ed152b8aabfd99679c42ecc0590f9f68a
Abenezer Mamo <abmamo@reed.edu>
2018-12-20 04:56:14 +0000

    modified test file to suit travis

---
+++ test_app.py
@@ -0,0 +1,18 @@
+import unittest
+import app
+
+class homepage_test(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+        self.app.testing = True
+
+    def test_main_page(self):
+         response = self.app.get('/', follow_redirects=True)
+         self.assertEqual(response.status_code, 200)
+
+    def test_nonexistent_page(self):
+         response = self.app.get('/azaz', follow_redirects=True)
+         self.assertEqual(response.status_code, 404)
+
+if __name__ == "__main__":
+    unittest.main()
