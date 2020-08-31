0e480bc5e45734a9f8d876b30d1dbfe1075c5c71
Abenezer Mamo <contact@abmamo.com>
2020-08-30 21:42:56 -0700

    updated new layout. only needs minor fixes

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ class ProductionConfig:
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'This is secret'
+
+class DevelopmentConfig:
+    DEBUG = True
+    TESTING = True
+    CSRF_ENABLED = False
+    SECRET_KEY = 'This is secret'
--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,34 +1,64 @@
-# Import dependencies
-from flask import Flask, render_template, request, flash, redirect, url_for, jsonify, send_from_directory, json
-import config
+# flask dependencies
+from flask import Flask, render_template, send_from_directory
+# environment
+from dotenv import load_dotenv
+# logging
+import logging
+# config
+import os
+# load environment
+load_dotenv()
+# get environment
+environment = os.environ.get("ENVIRONMENT")
 
-# Initialise app
+# init app
 app = Flask(__name__, static_url_path='/static')
-# Get configuration
-app.config.from_object('config.ProductionConfig')
-
-
+# config
+if environment == "production":
+    # configure app
+    app.config.from_object('config.ProductionConfig')
+    # configure app logger
+    logging.basicConfig(level=logging.ERROR)
+else:
+    # configure app
+    app.config.from_object('config.DevelopmentConfig')
+    # configure app logger
+    logging.basicConfig(level=logging.DEBUG)
+    
+# work page
 @app.route('/', methods=['GET'])
 def work():
     # error handling
     try:
         return render_template('work.html'), 200
-    except:
-        return render_template('500.html'), 500
-
+    except Exception as e:
+        # log
+        app.logger.error("getting work page failed.")
+        app.logger.error(str(e))
+        # return error template
+        return render_template('error.html'), 500
+# about page
 @app.route('/about', methods=['GET'])
 def about():
     try:
         return render_template('about.html'), 200
     except Exception as e:
-        return render_template('500.html'), 500
-
+        # log
+        app.logger.error("getting work page failed.")
+        app.logger.error(str(e))
+        # return error template
+        return render_template('error.html'), 500
+# resume page
 @app.route('/resume')
 def resume():
     try:
         return send_from_directory(app.static_folder, filename="resume.pdf")
     except Exception as e:
-        return render_template('500.html'), 500
+        # log
+        app.logger.error("getting work page failed.")
+        app.logger.error(str(e))
+        # return error template
+        return render_template('error.html'), 500
 
 # Routes for google analytics
 @app.route('/google51951de21c061dc9.html', methods=['GET'])
