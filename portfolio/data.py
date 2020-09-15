8ac25db4a7ff9ea9d32b45d9d5b823d3db1a099d
Abenezer Mamo <contact@abmamo.com>
2020-09-15 15:07:04 -0700

    updated data first draft. keep updating it

--- portfolio/data.py
+++ portfolio/data.py
@@ -4,36 +4,34 @@ work_text = [
         "title": "Junior Python Developer", 
         "company": "Pivony", 
         "duration": "May, 2020 - present", 
-        "summary": """worked on creating and maintaining a cloud native microservice infrastructure for topic modeling""", 
-        "projects": ["preprocessing engine", "aws resource orchestrator", "async email manager"],
-        "tools": ["AWS", "python", "dask", "docker", "flask", "gensim", "bash", "PostgreSQL"]
+        "summary": """building a cloud native and micro service oriented topic modeling infrastructure to efficiently process and summarize textual data and identify customer trends such as sentiment, complaints, influence, keywords, topics, and actionable insights.""", 
+        "projects": ["preprocessing engine", "aws resource orchestrator", "restful API"],
+        "tools": ["AWS", "python", "dask", "docker", "flask", "bash", "PostgreSQL", "git"]
     },
     {
         "title": "Topics in NLP", 
         "company": "Reed College/University of Sussex", 
         "duration": "Sep, 2017 - Jan, 2020", 
-        "summary": """researched various topics in NLP including named entity recognition, neural networks, databases, algorithms and data structures culminating in my final year dissertation scalable learning for the odd man out task with applications to word embedding induction where I explored the effect of transfer learning""", 
+        "summary": """researched various topics in CS including but not limited to genetic algorithms, neural networks, robotics, data manipulation, and database management culminating in a final year dissertation exploring the effect of incremental training on word embedding generation.""",
         "projects": ["genetic algorithms", "word embeddings", "NER engine"],
-        "tools": ["pyTorch", "python", "spaCy", "anaconda", "MySQL", "Haskell"]
+        "tools": ["pyTorch", "python", "spaCy", "anaconda", "MySQL", "Haskell", "git"]
     },
     {
         "title": "Full Stack Developer Intern", 
         "company": "Software Design Stuio", 
         "duration": "Jan, 2017 - Aug, 2017", 
-        "summary": "took part in an immersive program where I was introduced to the fundamentals of web programming using flask, javascript, CSS, HTML, postgreSQl resulting in a fleet management / geolocation tracking web application", 
-        "projects": ["geolocation tracking", "user authentication", "CSS Theme"],
-        "tools": ["python", "HTML", "JavaScript", "CSS", "PostgreSQL"]
+        "summary": """got introduced to the fundamentals of web programming using flask, javascript, postgreSQL, HTML/CSS/JavaScript resuling in a fleet management / geolocation tracking web application for efficient & accurate vehicle management""",
+        "projects": ["geolocation tracking", "user authentication", "CSS Themes"],
+        "tools": ["flask", "HTML", "JavaScript", "CSS", "PostgreSQL", "jinja", "git"]
     }
 ]
 
 about_text = [
         """
-            I am a back end developer with a strong background in web/API development (Flask/Django), databases (PostgreSQL/MySQL), cloud-native development
-            (AWS), NLP/ML (pyTorch/anaconda/jupyter), managing & extracting meaningful insight from large datasets (dask/pandas/parquet), devops & infrastructure
-            management (Linux/bash/systemd), & MVP prototyping and development.
+            I am a back-end developer with strong backgrounds in web/API/microservice development, database management, cloud-native applications, NLP/ML frameworks, 
+            devops / command-line tools, managing & extracting meaningful insight from larget datasets, & rapid MVP prototyping and development.
         """,
         """
-            I am passionate about utilizing emerging technologies to build 
-            I am passionate about utilizing emerging technologies to build robust infrasturucture to deliver incredible products.
+            I am passionate about problem solving & utilizing established and emerging technologies to build robust, scalable products.
         """
 ]
