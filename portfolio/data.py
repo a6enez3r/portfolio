567d01dfc2d7fe5d6b0c2e3b980a9f7a16779a37
Abenezer Mamo <contact@abmamo.com>
2020-09-20 01:20:25 -0700

    linting done. init styling done. added new color palette. fixed spacing issues. fully responsive

--- portfolio/data.py
+++ portfolio/data.py
@@ -1,37 +1,50 @@
 # work
 work_text = [
     {
-        "title": "Junior Python Developer", 
-        "company": "Pivony", 
-        "duration": "May, 2020 - present", 
-        "summary": """building a cloud native and micro service oriented topic modeling infrastructure to efficiently process and summarize textual data and identify customer trends such as sentiment, complaints, influence, keywords, topics, and actionable insights.""", 
-        "projects": ["preprocessing engine", "aws resource orchestrator", "restful API"],
-        "tools": ["AWS", "python", "dask", "docker", "flask", "bash", "PostgreSQL", "git"]
+        "title": "Junior Python Developer",
+        "company": "Pivony",
+        "duration": "May, 2020 - present",
+        "summary": """building a cloud native and micro service oriented topic modeling infrastructure to efficiently process and summarize textual data and identify customer trends such as sentiment, complaints, influence, keywords, topics, and actionable insights.""",
+        "projects": [
+            "preprocessing engine",
+            "aws resource orchestrator",
+            "restful API",
+        ],
+        "tools": [
+            "AWS",
+            "python",
+            "dask",
+            "docker",
+            "flask",
+            "bash",
+            "PostgreSQL",
+            "git",
+        ],
     },
     {
-        "title": "Topics in NLP", 
-        "company": "Reed College/University of Sussex", 
-        "duration": "Sep, 2017 - Jan, 2020", 
+        "title": "Topics in NLP",
+        "company": "Reed College/Univ. of Sussex",
+        "duration": "Sep, 2017 - Jan, 2020",
         "summary": """researched various topics in CS including but not limited to genetic algorithms, neural networks, robotics, data manipulation, and database management culminating in a final year dissertation exploring the effect of incremental training on word embedding generation.""",
         "projects": ["genetic algorithms", "word embeddings", "NER engine"],
-        "tools": ["pyTorch", "python", "spaCy", "anaconda", "MySQL", "Haskell", "git"]
+        "tools": ["pyTorch", "python", "spaCy", "anaconda", "MySQL", "Haskell", "git"],
     },
     {
-        "title": "Full Stack Developer Intern", 
-        "company": "Software Design Stuio", 
-        "duration": "Jan, 2017 - Aug, 2017", 
+        "title": "Full Stack Developer Intern",
+        "company": "Software Design Stuio",
+        "duration": "Jan, 2017 - Aug, 2017",
         "summary": """got introduced to the fundamentals of web programming using flask, javascript, postgreSQL, HTML/CSS/JavaScript resuling in a fleet management / geolocation tracking web application for efficient & accurate vehicle management""",
         "projects": ["geolocation tracking", "user authentication", "CSS Themes"],
-        "tools": ["flask", "HTML", "JavaScript", "CSS", "PostgreSQL", "jinja", "git"]
-    }
+        "tools": ["flask", "HTML", "JavaScript", "CSS", "PostgreSQL", "jinja", "git"],
+    },
 ]
 
 about_text = [
-        """
+    """
             I am a back-end developer with strong backgrounds in web/API/microservice development, database management, cloud-native applications, NLP/ML frameworks, 
             devops / command-line tools, managing & extracting meaningful insight from larget datasets, & rapid MVP prototyping and development.
         """,
-        """
+    """
             I am passionate about problem solving & utilizing established and emerging technologies to build robust, scalable products.
-        """
+        """,
 ]
