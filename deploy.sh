166cc6b0b922820b2d138454c343d12d1729cb88
Abenezer Mamo <contact@abmamo.com>
2020-09-21 18:52:49 -0700

    initial commit

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add "$HOME_DIR"/.ssh/id_rsa
 
-  cd $APP_DIR
+  cd "$APP_DIR"
   git pull origin master
 else
   echo "Not deploying, since this branch isn't master."
