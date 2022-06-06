3ceb6cc4979a947b5f9af282df28a5646c864ee0
abenezer <abenezer@abenezer.mac>
2022-06-05 19:58:57 -0700

    fix failing tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 """
     test_app.py: contains tests for pages
 """
+from unittest import mock
 
 
 def test_home(test_client):
@@ -17,10 +18,11 @@ def test_about(test_client):
     """
     test about page
     """
-    # get page
-    response = test_client.get("/about")
-    # assert response is valid
-    assert response.status_code == 200
+    with mock.patch("src.bps.pages.github_projects", return_value=[]):
+        # get page
+        response = test_client.get("/about")
+        # assert response is valid
+        assert response.status_code == 200
 
 
 def test_resume(test_client):
